bbb52a6edc8e2f1ed4308a2f8041f323f2aa681d
Ind-Hacker-262 <darshanrao200421@gmail.com>
2026-08-15 20:31:25 +0530

    Configure Vercel serverless deployment and clean requirements.txt

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from app import create_app
 
-application = create_app()
+app = create_app()
+application = app
 
 if __name__ == '__main__':
-    application.run(debug=True)
+    app.run(debug=True)
